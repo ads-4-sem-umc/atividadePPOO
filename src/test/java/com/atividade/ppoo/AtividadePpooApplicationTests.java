b52c67fa6ebb192e5d17fd08a2b5969afc11bce1
Ruan Dias <ruan.dias@muralis.com.br>
2024-10-31 21:07:35 -0300

    intial commit

---
+++ src/test/java/com/atividade/ppoo/AtividadePpooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividade.ppoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadePpooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
